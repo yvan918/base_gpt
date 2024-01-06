64e6dd8103e176c6a6dbb99adf08519fa79b5017
Replit user <>
2024-01-06 16:24:17 +0000

    one

--- app/components/chat.tsx
+++ app/components/chat.tsx
@@ -100,62 +100,62 @@ export function SessionConfigModel(props: { onClose: () => void }) {
     const maskStore = useMaskStore();
     const navigate = useNavigate();
 
-    // return (
-    //     <div className="modal-mask">
-    //         <Modal
-    //             title={Locale.Context.Edit}
-    //             onClose={() => props.onClose()}
-    //             actions={[
-    //                 <IconButton
-    //                     key="reset"
-    //                     icon={<ResetIcon />}
-    //                     bordered
-    //                     text={Locale.Chat.Config.Reset}
-    //                     onClick={async () => {
-    //                         if (await showConfirm(Locale.Memory.ResetConfirm)) {
-    //                             chatStore.updateCurrentSession(
-    //                                 (session) => (session.memoryPrompt = ""),
-    //                             );
-    //                         }
-    //                     }}
-    //                 />,
-    //                 <IconButton
-    //                     key="copy"
-    //                     icon={<CopyIcon />}
-    //                     bordered
-    //                     text={Locale.Chat.Config.SaveAs}
-    //                     onClick={() => {
-    //                         navigate(Path.Masks);
-    //                         setTimeout(() => {
-    //                             maskStore.create(session.mask);
-    //                         }, 500);
-    //                     }}
-    //                 />,
-    //             ]}
-    //         >
-    //             <MaskConfig
-    //                 mask={session.mask}
-    //                 updateMask={(updater) => {
-    //                     const mask = { ...session.mask };
-    //                     updater(mask);
-    //                     chatStore.updateCurrentSession((session) => (session.mask = mask));
-    //                 }}
-    //                 shouldSyncFromGlobal
-    //                 extraListItems={
-    //                     session.mask.modelConfig.sendMemory ? (
-    //                         <ListItem
-    //                             className="copyable"
-    //                             title={`${Locale.Memory.Title} (${session.lastSummarizeIndex} of ${session.messages.length})`}
-    //                             subTitle={session.memoryPrompt || Locale.Memory.EmptyContent}
-    //                         ></ListItem>
-    //                     ) : (
-    //                         <></>
-    //                     )
-    //                 }
-    //             ></MaskConfig>
-    //         </Modal>
-    //     </div>
-    // );
+    return (
+        <div className="modal-mask">
+            <Modal
+                title={Locale.Context.Edit}
+                onClose={() => props.onClose()}
+                actions={[
+                    <IconButton
+                        key="reset"
+                        icon={<ResetIcon />}
+                        bordered
+                        text={Locale.Chat.Config.Reset}
+                        onClick={async () => {
+                            if (await showConfirm(Locale.Memory.ResetConfirm)) {
+                                chatStore.updateCurrentSession(
+                                    (session) => (session.memoryPrompt = ""),
+                                );
+                            }
+                        }}
+                    />,
+                    <IconButton
+                        key="copy"
+                        icon={<CopyIcon />}
+                        bordered
+                        text={Locale.Chat.Config.SaveAs}
+                        onClick={() => {
+                            navigate(Path.Masks);
+                            setTimeout(() => {
+                                maskStore.create(session.mask);
+                            }, 500);
+                        }}
+                    />,
+                ]}
+            >
+                <MaskConfig
+                    mask={session.mask}
+                    updateMask={(updater) => {
+                        const mask = { ...session.mask };
+                        updater(mask);
+                        chatStore.updateCurrentSession((session) => (session.mask = mask));
+                    }}
+                    shouldSyncFromGlobal
+                    extraListItems={
+                        session.mask.modelConfig.sendMemory ? (
+                            <ListItem
+                                className="copyable"
+                                title={`${Locale.Memory.Title} (${session.lastSummarizeIndex} of ${session.messages.length})`}
+                                subTitle={session.memoryPrompt || Locale.Memory.EmptyContent}
+                            ></ListItem>
+                        ) : (
+                            <></>
+                        )
+                    }
+                ></MaskConfig>
+            </Modal>
+        </div>
+    );
 }
 
 function PromptToast(props: {
